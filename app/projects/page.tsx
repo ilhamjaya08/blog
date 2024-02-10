b4745224a96e1c18e26b8bc440c2d17dffea41f4
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-02-10 13:09:35 +0700

    p

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,14 +13,14 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-             Showcase of my project
+            Showcase your projects with a hero image (16 x 9)
           </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
             {projectsData.map((d) => (
               <Card
-                key={d.title}
+                
                 title={d.title}
                 description={d.description}
                 imgSrc={d.imgSrc}
